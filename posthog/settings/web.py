50d2822900857986b08308bd202518996f0956d7
Raúl Negrón <raul.n@posthog.com>
2026-05-18 15:11:21 -0400

    feat: scaffold engineering_analytics product

--- posthog/settings/web.py
+++ posthog/settings/web.py
@@ -75,6 +75,7 @@ PRODUCTS_APPS = [
     "products.warehouse_sources_queue.backend.apps.WarehouseSourcesQueueConfig",
     "products.business_knowledge.backend.apps.BusinessKnowledgeConfig",
     "products.deployments.backend.apps.DeploymentsConfig",
+    "products.engineering_analytics.backend.apps.EngineeringAnalyticsConfig",
 ]
 
 INSTALLED_APPS = [
